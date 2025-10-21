0a4d0f60270200ec028901210312678fa078a14f
Jise1212 <titocool334@gmail.com>
2025-10-20 22:53:35 -0500

    Update Calculadora.java
    implemente la suma

---
+++ Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+
+    // Suma dos enteros y devuelve el resultado
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    // Demo rápida
+    public static void main(String[] args) {
+        int r = suma(5, 7);
+        System.out.println("Resultado: " + r); // debe imprimir 12
+    }
+}
